b6c51c1e51b97a93e62162c47f786fd70a099810
vaishnavi-0613 <vaishnavibeenaveni@gmail.com>
2025-12-28 16:25:14 +0530

    Create App.js

---
+++ src/components/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MultiStepForm from "./components/MultiStepForm";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>User Registration</h1>
+      <MultiStepForm />
+    </div>
+  );
+}
+
+export default App;
